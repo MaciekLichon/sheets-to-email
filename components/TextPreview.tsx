82ae56d6542b8207b4070cfdcc88bb48202b7322
Maciej Lichon <maciek.lichon@gmail.com>
2026-01-09 21:08:48 +0100

    add copy to clipboard

--- components/TextPreview.tsx
+++ components/TextPreview.tsx
@@ -12,12 +12,21 @@ const TextPreview = ({ text, selectedRow }: TextPreviewProps) => {
       return value != null ? String(value) : `{{${key}}}`;
     });
 
+  const copyToClipboard = () => {
+    const transformedText = transformText(text, selectedRow);
+    navigator.clipboard.writeText(transformedText);
+    alert("Copied to clipboard!");
+  };
+
   return (
     <div
       id="preview"
       className="box-wrapper p-5 grid grid-rows-[auto_1fr] gap-1 overflow-auto"
     >
-      <h2>Preview</h2>
+      <div className="flex justify-between">
+        <h2>Preview</h2>
+        <button onClick={copyToClipboard}>Copy</button>
+      </div>
       <div className="whitespace-pre-wrap overflow-auto">
         {transformText(text, selectedRow)}
       </div>
